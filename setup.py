ccfb8979edab2f330262e513b07494f596e50019
Henrik Gabrielsen <henrik.v.gabrielsen@gmail.com>
2017-12-25 23:44:23 +0100

    inital commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="timmytime",
+    version="0.1",
+    author='Henrik V. Gabrielsen',
+    author_email='henrik.v.gabrielsen@gmail.com',
+    description='minimal timer wrapper with print or log',
+    packages=find_packages())
