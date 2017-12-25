ad05aa098d57fd3296b4b6c370339b9490f239e0
Henrik Gabrielsen <henrik.v.gabrielsen@gmail.com>
2017-12-26 00:28:24 +0100

    added email and url

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ setup(
     name="timmytime",
     version="0.1",
     author='Henrik V. Gabrielsen',
-    author_email='henrik.v.gabrielsen@gmail.com',
+    author_email='henrik.v.gabrielsen+timmytime@gmail.com',
+    url='https://github.com/hvgab/timmytime',
     description='minimal timer wrapper with print or log',
-    packages=find_packages())
+    license='MIT',
+    packages=['timmytime']
+    )
